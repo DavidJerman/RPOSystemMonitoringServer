7a07c1e7044e297cfc47ec65e9443007f148f3ad
David <45101011+DavidJerman@users.noreply.github.com>
2022-12-01 09:32:55 +0100

    Removed test code

--- main.cpp
+++ main.cpp
@@ -6,15 +6,7 @@
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
 
-    // Server server;
-
-    auto data = Protocol::UTF8JsonFromFile(R"(C:\QtBuilds\build-SystemMonitoringServer-Desktop_Qt_6_4_0_MinGW_64_bit-Debug\jsonExamples\newUpdateSystem.json)");
-
-    auto doc = Protocol::UTF8ToJsonDocument(data);
-    auto system = Protocol::jsonDocumentToSystem(doc);
-    auto doc2 = Protocol::systemToJsonDocument(system);
-    auto text = Protocol::jsonDocumentToUTF8(doc2);
-    Protocol::UTF8JsonToFile(text, R"(C:\QtBuilds\build-SystemMonitoringServer-Desktop_Qt_6_4_0_MinGW_64_bit-Debug\jsonExamples\test.json)");
+    Server server;
 
     return QCoreApplication::exec();
 }
